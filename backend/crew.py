b997367b5300dd6fa6b8f8c7969841afad5ad37e
main <claudia.raphael73@gmail.com>
2025-09-20 15:24:22 -0300

    Create context for the research task. created a list called background_info that is being called in the context parameter

--- backend/crew.py
+++ backend/crew.py
@@ -2,10 +2,13 @@ import os
 from crewai import Agent, Task, Crew, Process
 from crewai_tools import SerperDevTool
 
-# Tools
+#### Tools
 search_tool = SerperDevTool()
+analysis_tool = SerperDevTool()
 
-# Agents
+
+
+#### Agents
 researcher = Agent(
     role="AI Research Specialist",
     goal="Discover the latest AI trends about {topic}",
@@ -45,7 +48,8 @@ writer = Agent(
     allow_delegation=False,
 )
 
-# Tasks
+#### Tasks
+
 research_task = Task(
     description=(
         "Research the latest developments and insights about {topic}. Summarize 3–5 main points."
@@ -53,6 +57,30 @@ research_task = Task(
     expected_output="A structured summary of 3-5 main points about {topic}.",
     agent=researcher,
     tools=[search_tool],
+    background_info=[
+        "Search the web for the latest news about {topic} from the last 30 days.",
+        "Search specifically for news from the last 2 weeks first for the first 3 bullet points of the 5 you will write",
+        "stick to your task of researching the latest news about {topic}, and summarize eah piece of news you find into 5 bullet points",
+        "The last 2 bulletpoints should be reserved for major news in the last 30 days, such as information relating to significant news about FAANG, Openai and the Chinese, Deepseek. We are seeking news like new products, new features, new models, new metrics, new technology, microchip production, datacenters."     
+     ],
+    context=[background_info]
+)
+
+analysis_task = Task(
+    description=(
+        "Analyse the data provided by the AI Research Specialist and provide insights."
+        ),
+    agent=analyst,
+    tools=[analysis_tool],
+    context=[research_task],
+
+)
+
+rationale_task = Task(
+    description=("Understand the analysis of the Data Analyst and explain the rational decisions behind it's analysis' results."),
+    agent=Rationalist,
+    tools=[analysis_tool],
+    context=[analysis_task],
 )
 
 write_task = Task(
